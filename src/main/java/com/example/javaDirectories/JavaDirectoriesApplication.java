26cd767aacc679725d684415ad86b54623d21869
John <ashton.basile@gmail.com>
2023-10-02 19:17:58 +0100

    made the project on start.spring.io

---
+++ src/main/java/com/example/javaDirectories/JavaDirectoriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaDirectories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDirectoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDirectoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaDirectories/JavaDirectoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaDirectories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDirectoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
